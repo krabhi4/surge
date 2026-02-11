089e2f145f9c8664cd398266f87fe909a66da91f
junaid2005p <75373245+junaid2005p@users.noreply.github.com>
2026-02-12 01:07:23 +0530

    Settings bug (#154)
    * refactor: Removed all mentions of global connection limit because we do not currently use it anywhere
    
    * refactor: Updated the tests to remove global Connection limit
    
    * fix: Proxy URL was not being copied in config_convert making the entire proxy functionality not being actually useful. Added the proxyURL to be returned in types.RuntimeConfig used by the downloader
    
    * test Added tests to ensure all fields are copied properly into the runtime config used by the downloader
    
    * chore: removed commented code from root.go

--- internal/engine/types/config_convert.go
+++ internal/engine/types/config_convert.go
@@ -6,8 +6,8 @@ import "github.com/surge-downloader/surge/internal/config"
 func ConvertRuntimeConfig(rc *config.RuntimeConfig) *RuntimeConfig {
 	return &RuntimeConfig{
 		MaxConnectionsPerHost: rc.MaxConnectionsPerHost,
-		MaxGlobalConnections:  rc.MaxGlobalConnections,
 		UserAgent:             rc.UserAgent,
+		ProxyURL:              rc.ProxyURL,
 		SequentialDownload:    rc.SequentialDownload,
 		MinChunkSize:          rc.MinChunkSize,
 		WorkerBufferSize:      rc.WorkerBufferSize,
